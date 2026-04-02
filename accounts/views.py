d8cbe4d401277cf4da2268a97e2950c5f2d36dfe
Arash <arrashdashtim@gmail.com>
2026-04-02 20:20:41 +0330

    set up frontend

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
+# Create your views here
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "user created"}, status=201)
+
+        return Response(serializer.errors, status=400)
